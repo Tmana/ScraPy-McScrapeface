9adacaaf2dff954aa2e3116d95ba7bb2d3de2f91
Tmana <FLCL.gmana@gmail.com>
2017-05-31 10:26:52 -0400

    changed license fron GNU, to basic proprietary to Proplogix.

--- manatee_pdf_grab.py
+++ manatee_pdf_grab.py
@@ -1,19 +1,7 @@
  #    © Tanner Robart 2017
 
- #    This file is part of Scrapy-McScrapeface.
-
- #    Scrapy-McScrapeface is free software: you can redistribute it and/or modify
- #    it under the terms of the GNU General Public License as published by
- #    the Free Software Foundation, either version 3 of the License, or
- #    (at your option) any later version.
-
- #    Scrapy-McScrapeface is distributed in the hope that it will be useful,
- #    but WITHOUT ANY WARRANTY; without even the implied warranty of
- #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- #    GNU General Public License for more details.
-
- #    You should have received a copy of the GNU General Public License
- #    along with Scrapy-McScrapeface.  If not, see <http://www.gnu.org/licenses/>.
+#    This file is part of Scrapy-McScrapeface. It is owned and licensed by Proplogix. Any use of this code must be explicitly negotiated with Proplogix.
+#    Any use of this code must be explicitly negotiated with Proplogix.
 
 import urllib
 import urllib.request
--- sarasota_pdf_grab.py
+++ sarasota_pdf_grab.py
@@ -1,17 +1,8 @@
  #    © Tanner Robart 2017
 
- #    This file is part of Scrapy-McScrapeface.
+#    This file is part of Scrapy-McScrapeface. It is owned and licensed by Proplogix. 
+#    Any use of this code must be explicitly negotiated with Proplogix.
 
- #    Scrapy-McScrapeface is free software: you can redistribute it and/or modify
- #    it under the terms of the GNU General Public License as published by
- #    the Free Software Foundation, either version 3 of the License, or
- #    (at your option) any later version.
-
- #    Scrapy-McScrapeface is distributed in the hope that it will be useful,
- #    but WITHOUT ANY WARRANTY; without even the implied warranty of
- #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- #    GNU General Public License for more details.
- #    For more details, see <http://www.gnu.org/licenses/>.
 
 import urllib
 import urllib.request
